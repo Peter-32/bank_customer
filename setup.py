90a2f0a710047b737f483dcecc839f1f50cb42ac
Peter-32 <peter@impactradius.com>
2019-08-12 16:13:08 -0700

    init

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='The classification goal is to predict if the client will make a particular cash investment with the bank.',
+    author='Peter Myers',
+    license='MIT',
+)
