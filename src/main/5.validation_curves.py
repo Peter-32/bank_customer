b4561c6e70654c499d9f5fa60e42092896a0e989
Peter-32 <peter@impactradius.com>
2019-08-28 09:14:30 -0700

    a

---
+++ src/main/5.validation_curves.py
@@ -0,0 +1,13 @@
+# Add this project to the path
+import os; import sys; currDir = os.path.dirname(os.path.realpath("__file__"))
+rootDir = os.path.abspath(os.path.join(currDir, '..')); sys.path.insert(1, rootDir)
+
+import warnings
+warnings.filterwarnings("ignore")
+
+# My modules
+from models.train_model import *
+from features.build_features import *
+
+features_pipeline = data_preparation()
+train_model(features_pipeline)
--- src/models/train_model.py
+++ src/models/train_model.py
@@ -4,9 +4,10 @@ warnings.filterwarnings("ignore")
 # Public modules
 import numpy as np
 from pandas import read_csv
+import matplotlib.pyplot as plt
 from sklearn.pipeline import Pipeline
 from sklearn.linear_model import LogisticRegression
-from sklearn.cross_validation import cross_val_score
+from sklearn.model_selection import cross_val_score
 
 def train_model(features_pipeline):
     train = read_csv("../../data/interim/train.csv")
@@ -16,19 +17,7 @@ def train_model(features_pipeline):
         ("clf", LogisticRegression(random_state=1)),
     ])
 
-
     scores = cross_val_score(estimator=full_pipeline, X=train, y=train_y,
                              cv=10, n_jobs=1)
     print('CV accuracy scores: %s' % scores)
-    print('CV accuracy: %.3f +/- %.f' % (np.mean(scores), np.std(scores)))
-
-
-
-
-
-
-
-
-
-    pipe_lr = full_pipeline.fit(train, train_y)
-    print("Test Accuracy: %.3f" % pipe_lr.score(train, train_y))
+    print('CV accuracy: %.3f +/- %.3f' % (np.mean(scores), np.std(scores)))
